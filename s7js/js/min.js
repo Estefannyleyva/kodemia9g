922343ac1a48f2110d7315217158a730b94eb277
Estefannyleyva <fanniluley88@gmail.com>
2020-08-22 11:10:01 -0500

    agregando practicas

---
+++ s7js/js/min.js
@@ -0,0 +1,10 @@
+let fras = "La mejor manera de predecir el futuro es creándolo";
+let i = 0;
+let nfrass = ""
+while (i < frass.length){
+    if(i % 3 === 0){
+        nfrass += frass[i];
+    }
+    i++;
+}
+console.log(frass);
